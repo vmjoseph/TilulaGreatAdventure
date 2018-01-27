46547750ba7322c49fcbe9c4c41de878eeead0ac
vmjoseph <valliejoseph@gmail.com>
2018-01-26 20:04:49 -0500

    initial commit

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1517014992,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/9patch.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/papers-sheet0.png",
+		"images/letter-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
